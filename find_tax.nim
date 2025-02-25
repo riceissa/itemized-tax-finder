8e606b03d9a4e06a34b68ae3a30006b5e99ee194
Issa Rice <riceissa@gmail.com>
2025-02-24 16:54:12 -0800

    better function signatures

--- find_tax.nim
+++ find_tax.nim
@@ -13,6 +13,10 @@ type
         tax: float
     History = object
         hist: seq[TaxedValue]
+    FinalResult = object
+        hist: History
+        round_before_summing: bool
+        sum: float
 
 func newHistory(): History =
     return History(hist: @[])
@@ -37,10 +41,9 @@ func sum(hist: History, round_before_summing: bool): float =
 proc `$`(hist: History): string =
     result = hist.hist.join(", ")
 
-proc calculate_taxes(amounts: seq[float], total: float, tax_rates: seq[float]): string =
-    result = ""
-    let tax_multipliers = tax_rates
-    for t in tax_multipliers:
+proc calculate_taxes(amounts: seq[float], total: float, tax_rates: seq[float]): seq[FinalResult] =
+    result = @[]
+    for t in tax_rates:
         # The tax multiplier must be consistent within a history, so we only
         # define possible_hists once we are inside of the loop.
 
@@ -59,11 +62,7 @@ proc calculate_taxes(amounts: seq[float], total: float, tax_rates: seq[float]): 
             for round_before_summing in [true, false]:
                 let hist_sum = hist.sum(round_before_summing)
                 if abs(hist_sum - total) < 0.01 - 0.00001:
-                    result &= "  *"
-                    result &= " round before summing? " & (if round_before_summing: "yes" else: "no")
-                    result &= fmt", {hist}"
-                    result &= fmt": total was {hist_sum}"
-                    result &= "\n"
+                    result.add(FinalResult(hist: hist, round_before_summing: round_before_summing, sum: hist_sum))
 
 func parse_float_seq(input: string): Result[seq[float], string] =
     var res: seq[float] = @[]
@@ -81,7 +80,7 @@ func parse_single_float(input: string): Result[float, string] =
     except ValueError as e:
         return err(e.msg)
 
-func parse_input_and_calc(amounts_input, total_input, tax_rates_input: string): Result[string, string] =
+func parse_input_and_calc(amounts_input, total_input, tax_rates_input: string): Result[seq[FinalResult], string] =
     # It would be better if I could have the return type of this function be
     # just string, and then inside this function use do-notation to write
     # something like:
@@ -91,26 +90,37 @@ func parse_input_and_calc(amounts_input, total_input, tax_rates_input: string): 
     #         tax_rates <- parse_float_seq(tax_rates_input)
     #         ok(calculate_taxes(amounts, total, tax_rates))
     #     if res.isOk:
-    #         return res.get()
+    #         return (... message is defined here ...)
     #     else:
     #         return res.error()
-    # Unfortunately, I don't think this is possible in Nim using the Results
-    # package. That's because the early return operator ? will just return out
+    # This way, there would only be a single function that the
+    # user has to call. Unfortunately, I don't think this is
+    # possible in Nim using the Results package.
+    # That's because the early return operator ? will just return out
     # of the *whole function*, rather than just doing an early return out to
     # the "do-block" portion. So I need to have two different functions (this
-    # one and unpack_message below) just to contain the ? operator. This
-    # function is basically the do-block, and unpack_message is the outer
-    # function that uses the do-block's result.
+    # one and pack_message below) just to contain the ? operator. This
+    # function is basically the do-block, and pack_message is the outer
+    # function that uses the do-block's result.  Maybe when I am more
+    # knowledgeable about Nim I will be able to write the proper template/macro
+    # that does what I need.
     let amounts = ?parse_float_seq(amounts_input)
     let total = ?parse_single_float(total_input)
     let tax_rates = ?parse_float_seq(tax_rates_input)
     return ok(calculate_taxes(amounts, total, tax_rates))
 
-func unpack_message(message: Result[string, string]): string =
-    if message.isOk:
-        return "Results:\n" & message.get()
+func pack_message(calculated: Result[seq[FinalResult], string]): string =
+    if calculated.isOk:
+        var message = "Results:\n"
+        for res in calculated.get():
+            message &= "  *"
+            message &= " round before summing? " & (if res.round_before_summing: "yes" else: "no")
+            message &= fmt", {res.hist}"
+            message &= fmt": total was {res.sum}"
+            message &= "\n"
+        return message
     else:
-        return "Could not parse input:\n" & message.error()
+        return "Could not parse input:\n" & calculated.error()
 
 when not defined(js):
     proc main() =
@@ -145,11 +155,11 @@ when not defined(js):
             echo "You must pass in all three of amounts, total, and tax rates using the flags (the values are for example) --amounts=22.09,81.89,16.24 --total=124.13 --tax-rates=1.100,1.101,1.102,1.103"
             quit()
 
-        echo unpack_message(parse_input_and_calc(amounts_input, total_input, tax_rates_input))
+        echo pack_message(parse_input_and_calc(amounts_input, total_input, tax_rates_input))
 
     if isMainModule:
         main()
 
 when defined(js):
     proc runJS(amounts_input: cstring, total_input: cstring, tax_rates_input: cstring): cstring {.exportc} =
-        return cstring(unpack_message(parse_input_and_calc($amounts_input, $total_input, $tax_rates_input)))
+        return cstring(pack_message(parse_input_and_calc($amounts_input, $total_input, $tax_rates_input)))
